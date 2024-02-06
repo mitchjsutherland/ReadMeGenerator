6b19504c5a6b8ccade8533a3326ed0dbbffb5244
Mitch Sutherland <mitchelsutherland@icloud.com>
2024-02-06 22:24:14 +0000

    Dynamic badges added

--- index.js
+++ index.js
@@ -84,8 +84,8 @@ function questionUser() {
                     // short:
                 },
                 {
-                    name: 'GNU Lesser General Public License v2.1',
-                    value: 'GNU Lesser General Public License v2.1',
+                    name: 'GNU Lesser General Public License v3.0',
+                    value: 'GNU Lesser General Public License v3.0',
                     // short: 
                 },
                 {
@@ -96,6 +96,11 @@ function questionUser() {
                 {
                     name: 'The Unilicense',
                     value: 'The Unilicense'
+                    // short:
+                },
+                {
+                    name: 'The WTF***PL',
+                    value: 'The WTF***PL'
                     // short:
                 },
             ],
@@ -121,14 +126,84 @@ function questionUser() {
 };
 
 
+// Determine the selected license and set the badge link accordingly
+
+
 
 // Function to create user README content 
 
 function createReadMeContent(answers) {
 
+    let licenseBadge;
+
+    switch (answers.license) {
+        case 'Apache License 2.0':
+            licenseBadge = '![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+            break;
+
+        case 'GNU General Public License v3.0':
+            licenseBadge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+            break;
+
+        case 'MIT':
+            licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+            break;
+
+        case 'BSD 2-Clause "Simplified" License':
+            licenseBadge = '![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)';
+            break;   
+
+        case 'BSD 3-Clause "New" or "Revised License':
+            licenseBadge = '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
+            break;
+
+        case 'Boost Software License':
+            licenseBadge = '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
+            break;
+
+        case 'Creative Commons Zero v1.0 Universal':
+            licenseBadge = '![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)';
+            break; 
+
+        case 'Eclipse Publish License 2.0':
+            licenseBadge = '![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)';
+            break; 
+
+        case 'GNU Affero General Public License v3.0':
+            licenseBadge = '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)';
+            break; 
+
+        case 'GNU General Public License v2.0':
+            licenseBadge = '![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+            break; 
+
+        case 'GNU Lesser General Public License v3.0':
+            licenseBadge = '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)';
+            break; 
+
+        case 'Mozilla Public License 2.0':
+            licenseBadge = '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)';
+            break; 
+
+        case 'The Unilicense':
+            licenseBadge = '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+            break;
+
+        case 'The WTF***PL':
+            licenseBadge = '![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)';
+            break;
+
+        default:
+            licenseBadge = '';
+
+    }
+
+
 let fileContent = 
 
 `# ${answers.title}
+
+${licenseBadge}
 
 ## Description
 ${answers.description}
@@ -149,6 +224,8 @@ ${answers.usage}
 
 ## License
 The license for this project is ${answers.license}.
+
+
 
 ## Contributing
 [${answers.contribution}](https://github.com/${answers.contribution})
@@ -171,7 +248,7 @@ async function createReadMe() {
     let answers = await questionUser();
     let content = createReadMeContent(answers);
     // console.log(content);
-    await fs.writeFile('ReadMeTesting3.md', content, (error) =>
+    await fs.writeFile('ReadMeTesting4.md', content, (error) =>
     error ? console.error(error) : console.log('Success! Your read me has been saved.'))
 };
 
